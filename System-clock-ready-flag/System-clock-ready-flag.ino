81a62ed9493aa3539eeb2feb9845fd0699de58d2
Contel87 <luigimdg@hotmail.it>
2024-03-29 09:34:15 +0100

    Create System-clock-ready-flag.ino

---
+++ System-clock-ready-flag/System-clock-ready-flag.ino
@@ -0,0 +1,20 @@
+/*--- Quando un segnale di Clock e' stabile e pronto da usare porta a 1 il suo relativo flag ---*/
+/*--- (8.2.12 Renesas User's Manual: Hardware) ---*/
+
+#define OSCSF ((volatile unsigned char *)0x4001E03C) 
+uint8_t reg = *OSCSF;
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+
+  bool HOCO = bitRead(reg, 0); // bit 0
+  Serial.print("HOCO Stabilization Flag: ");Serial.println(HOCO); //Internal Clock oscillator is ready!
+  bool MOSC = bitRead(reg, 3); // bit 3
+  Serial.print("MOSC Stabilization Flag: ");Serial.println(MOSC);//Main clock oscillator is ready!
+  bool PLL = bitRead(reg, 5); // bit 5
+  Serial.print("PLL Stabilization Flag: ");Serial.println(PLL);//PLL Clock Oscillation is ready!
+}
+
+void loop() {
+}
