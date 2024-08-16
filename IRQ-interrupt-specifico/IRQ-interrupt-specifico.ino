2e69482e158600500b83208daa3860db934d6c18
Contel87 <luigimdg@hotmail.it>
2024-08-16 16:05:00 +0200

    Update IRQ-interrupt-specifico.ino

--- IRQ-interrupt-specifico/IRQ-interrupt-specifico.ino
+++ IRQ-interrupt-specifico/IRQ-interrupt-specifico.ino
@@ -4,6 +4,39 @@ Esempio di come collegare un interrupt ad una funzione:
 Nel setup viene dichiarato l'interrupt da intercettare e la funzione da richiamare in caso di interrupt ... (in questo caso il pin D2 di R4 MINIMA a GND), per la lista completa vedere il file bsp_elc.h nel core di arduino;
 Successivamente, sempre nel setup viene configurata la porta P105 (pin D2 di R4 MINIMA) per funzionare da interrupt;
 In sostanza ogni volta che il pin D2 e' collegato a GND(ad esempio con un pulsante) viene eseguita la relativa funzione (in questo caso Myirq0_callback())
+
+IMPORTANT notice: "Cortex-M4 Devices Generic user guide" show a table of IRQ priority, user IRQ0 start from 12 :
+Priority --> Process
+-3             Reset
+-2             NMI (Non Mascable Interrupt)
+-1             HardFault
+ 0             MemManage
+ 1             BusFault
+ 2             UsageFault
+ 3             Reserved
+ 4             Reserved
+ 5             Reserved
+ 6             Reserved
+ 7             SVCall
+ 8             Reserved
+ 9             Reserved
+ 10            PendSV
+ 11            SysTick
+ 12            Interrupt(IRQ)  --> User IRQ priority start from here (12)
+ N             Interrrupt(IRQ)
+
+* This code is "AS IS" without warranty or liability. 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
 */
 
 #include "IRQManager.h"
@@ -33,7 +66,7 @@ void loop() {
 }
 
 void pin2IQR(){
-GenericIrqCfg_t cfg = {FSP_INVALID_VECTOR, 12, ELC_EVENT_ICU_IRQ0};
+GenericIrqCfg_t cfg = {FSP_INVALID_VECTOR, 12, ELC_EVENT_ICU_IRQ0}; // 12 is priority ... seet note1
 IRQManager::getInstance().addGenericInterrupt(cfg, Myirq0_callback);
 }
 
